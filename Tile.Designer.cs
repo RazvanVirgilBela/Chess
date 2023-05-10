6dd3477b4d3c5efa870c37b3d2f172e0d2a32db2
Razvan Bela <razvanvirgil.bela@gmail.com>
2023-05-10 10:31:32 +0300

    ChessGame

---
+++ Tile.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Chess
+{
+    partial class Tile
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PieceImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PieceImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PieceImage
+            // 
+            this.PieceImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PieceImage.BackColor = System.Drawing.Color.White;
+            this.PieceImage.ErrorImage = null;
+            this.PieceImage.InitialImage = null;
+            this.PieceImage.Location = new System.Drawing.Point(0, 0);
+            this.PieceImage.Margin = new System.Windows.Forms.Padding(2);
+            this.PieceImage.Name = "PieceImage";
+            this.PieceImage.Size = new System.Drawing.Size(70, 70);
+            this.PieceImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.PieceImage.TabIndex = 0;
+            this.PieceImage.TabStop = false;
+            // 
+            // Tile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PieceImage);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Tile";
+            this.Size = new System.Drawing.Size(68, 68);
+            this.Load += new System.EventHandler(this.Tile_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PieceImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox PieceImage;
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Chess
+{
+    public enum ChessColor { WHITE, BLACK, NONE }
+    public partial class Tile : UserControl
+    {
+        public ChessPiece piece = new ChessPiece(PieceKind.EMPTY); //EMPTY piece
+        public ChessColor color = ChessColor.WHITE;
+        public Location location = new Location();
+        public ChessColor tileAttack = ChessColor.NONE;
+        public void SetAttack(ChessPiece pieceAttack)
+        {
+            tileAttack = pieceAttack.color;
+        }
+        public void RemoveAttack()
+        {
+            tileAttack = ChessColor.NONE;
+        }
+        public void PrintTileInfo()
+        {
+            Debug.Print("Y:" + GetY + " X:" + GetX + " TileAttack:" + tileAttack.ToString());
+        }
+        public byte GetY { get { return location.y; } }
+        public byte GetX { get { return location.x; } }
+        public bool isOccupied() => piece.piecekind != PieceKind.EMPTY;
+        public Tile(byte y, byte x)
+        {
+            InitializeComponent();
+            location.y = y;
+            location.x = x;
+            Location = new Point(x * Size.Width, y * Size.Height);
+        }
+        public Tile(byte y, byte x, ChessColor color) : this(y, x)
+        {
+            this.color = color;
+        }
+        public Color TileColor() => color != ChessColor.WHITE ? Color.Gray : Color.LightGray;
+        public void PieceAssign(ChessPiece piece)
+        {
+            this.piece = piece;
+            PieceImage.BackgroundImage = PieceImages[piece.ImageName()];
+        }
+        private void Tile_Load(object sender, EventArgs e)
+        {
+            this.PieceImage.BackColor = TileColor();
+            this.BackColor = TileColor();
+            this.Click += TileClicked;
+            this.PieceImage.Click += TileClicked;
+        }
+        static int click = 0;
+        static Movement piecemoves;
+        public void PossibleMove(bool show)
+        {
+            if (!show) PieceImage.Image = null;
+            else PieceImage.Image = Properties.Resources.PossibleMove;
+        }
+        private void TileClicked(object sender, EventArgs e)
+        {
+            if (click == 0 && this.piece.color != Board.CurrentPlayer) return;
+            click++;
+            if (click == 1)
+            {
+                piecemoves = new Movement(this);
+                piecemoves.IsKingSafe();
+                piecemoves.MovesInterface(true);
+            }
+            else
+            {
+                piecemoves.isAvailableMove(this);
+                click = 0;
+            }
+        }
+        public static Dictionary<string, Image> PieceImages = new Dictionary<string, Image> {
+            { "BAngel",Properties.Resources.BAngel },
+            { "BBishop",Properties.Resources.BBishop},
+            { "BKnight",Properties.Resources.BKnight },
+            { "BRook",Properties.Resources.BRook },
+            { "BKing",Properties.Resources.BKing },
+            { "BQueen",Properties.Resources.BQueen },
+            { "BPawn",Properties.Resources.BPawn },
+            { "WAngel",Properties.Resources.WAngel },
+            { "WBishop",Properties.Resources.WBishop },
+            { "WKnight",Properties.Resources.WKnight },
+            { "WRook",Properties.Resources.WRook },
+            { "WKing",Properties.Resources.WKing},
+            { "WQueen",Properties.Resources.WQueen },
+            { "WPawn",Properties.Resources.WPawn },
+            { "EMPTY",null }
+        };
+    }
+    public struct Location
+    {
+        public byte y, x;
+    }
+}
